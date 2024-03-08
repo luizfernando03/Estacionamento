1e7701e9e05ef26e511d02171767a3cb68edba63
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-03-08 09:21:44 -0300

    Criando classe Estacionamento

---
+++ src/main/java/com/ada/exercicios/Estacionamneto.java
@@ -0,0 +1,4 @@
+package com.ada.exercicios;
+
+public class Estacionamneto {
+}
--- src/main/java/com/ada/exercicios/Veiculo.java
+++ src/main/java/com/ada/exercicios/Veiculo.java
@@ -1,4 +1,19 @@
 package com.ada.exercicios;
 
 public class Veiculo {
+    private String placa;
+    private String Tipo;
+
+    public Veiculo(String placa, String tipo){
+        this.placa = placa;
+        this.placa = tipo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
 }
