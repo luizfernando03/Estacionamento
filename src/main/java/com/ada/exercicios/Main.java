92e4648d05049bc4580abe8b99ea4914657eaea7
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-03-08 10:16:33 -0300

    Adicionando metodo na main

--- src/main/java/com/ada/exercicios/Main.java
+++ src/main/java/com/ada/exercicios/Main.java
@@ -2,6 +2,22 @@ package com.ada.exercicios;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Veiculo veiculo = new Veiculo("1234", "Carro");
+
+        Estacionamneto estacionamneto = new Estacionamneto();
+        estacionamneto.entrar(veiculo);
+
+        boolean existe = estacionamneto.consultar("1234");
+
+        if (existe){
+            System.out.println("O veiculo esta no estacionamento");
+        }else{
+            System.out.println("O veiculo não esta no estacionamento");
+        }
+
+
     }
+
+
 }
