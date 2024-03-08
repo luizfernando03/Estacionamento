14795517945cd7da7fa649c15fbce3a20cb97a66
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-03-08 10:09:23 -0300

    Adicionando dados na classe Estacionamento

--- src/main/java/com/ada/exercicios/Estacionamneto.java
+++ src/main/java/com/ada/exercicios/Estacionamneto.java
@@ -1,4 +1,38 @@
 package com.ada.exercicios;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Estacionamneto {
+
+    private List<Veiculo> veiculos;
+
+    public Estacionamneto(){
+        veiculos = new ArrayList<>();
+    }
+
+    public void entrar (Veiculo veiculo){
+        veiculos.add(veiculo);
+    }
+
+    public Veiculo sair(String placa){
+        Veiculo veiculoASair = null;
+        for (Veiculo veiculo : veiculos) {
+            if (veiculo.getPlaca().equals(placa)){
+                veiculoASair = veiculo;
+                break;
+            }
+        }
+        veiculos.remove(veiculoASair);
+        return veiculoASair;
+    }
+    public boolean consultar(String placa){
+        for (Veiculo veiculo : veiculos){
+            if (veiculo.getPlaca().equals(placa)){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
